68ac44fb8715ed5398d0d0aeb99c2f0817eae12e
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-03-23 08:37:41 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Juliano RAHAJAHARIMANGA on 23/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
