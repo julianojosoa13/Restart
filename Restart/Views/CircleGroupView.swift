3cd612f067301863db1fe7b448f09e952779714b
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-03-31 12:55:08 +0300

    CircleGroupView: Blur,opacity and scale animation

--- Restart/Views/CircleGroupView.swift
+++ Restart/Views/CircleGroupView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct CircleGroupView: View {
     @State var shapeColor: Color
     @State var shapeOpacity: Double
+    @State private var isAnimating: Bool = false
+    
     
     var body: some View {
         ZStack {
@@ -19,7 +21,16 @@ struct CircleGroupView: View {
             Circle()
                 .stroke(shapeColor.opacity(shapeOpacity), lineWidth: 80)
                 .frame(width: 260, height: 260, alignment: .center)
-        }    }
+        }
+        .blur(radius: isAnimating ? 0 : 10)
+        .opacity( isAnimating ? 1 : 0)
+        .scaleEffect(isAnimating ? 1:0.5)
+        .animation(.easeOut(duration: 1), value: isAnimating)
+        .onAppear(perform: {
+            isAnimating = true
+        })
+    }
+    
 }
 
 #Preview {
